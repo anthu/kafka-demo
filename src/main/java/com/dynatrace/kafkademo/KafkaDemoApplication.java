da2b24413255c3c65e4e1b987a1f0b2679916cbc
Anton Huck <antonhuck@gmail.com>
2021-05-17 09:44:47 +0200

    Create producer and consumer

--- src/main/java/com/dynatrace/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/dynatrace/kafkademo/KafkaDemoApplication.java
@@ -1,7 +1,15 @@
 package com.dynatrace.kafkademo;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
@@ -10,4 +18,26 @@ public class KafkaDemoApplication {
 		SpringApplication.run(KafkaDemoApplication.class, args);
 	}
 
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("topic1")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			while (true){
+				template.send("topic1", (new Date()).toString());
+				Thread.sleep(5000);
+			}
+		};
+	}
+
+	@KafkaListener(id = "example-listener", topics = "topic1")
+	public void listen(String in) {
+		System.out.println(in);
+	}
 }
