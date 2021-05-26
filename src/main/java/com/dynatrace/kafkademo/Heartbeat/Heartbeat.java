5162bb5027db816b97c9b1160f6b1279c6d25800
Anton Huck <antonhuck@gmail.com>
2021-05-26 14:55:33 +0200

    Use Spring Boot Scheduler for Heartbeat

--- src/main/java/com/dynatrace/kafkademo/Heartbeat/Heartbeat.java
+++ src/main/java/com/dynatrace/kafkademo/Heartbeat/Heartbeat.java
@@ -6,10 +6,11 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -17,6 +18,9 @@ import java.util.Date;
 @Component
 public class Heartbeat {
     Tracer tracer = GlobalOpenTelemetry.getTracer("HeartbeatTracer");
+
+    @Autowired
+    KafkaTemplate<String, String> template;
 
     @Bean
     public NewTopic heartbeatTopic() {
@@ -26,15 +30,14 @@ public class Heartbeat {
                 .build();
     }
 
-    @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
-        return args -> {
-            Span span = tracer.spanBuilder("runnerSpan").startSpan();
-            while (true) {
-                template.send(Constants.HEARTBEAT_TOPIC, (new Date()).toString());
-                Thread.sleep(5000);
-            }
-        };
+    @Scheduled(fixedRate = 5000)
+    public void heartbeatSchdule() {
+        Span span = tracer.spanBuilder("runnerSpan").startSpan();
+        String timestamp = (new Date()).toString();
+        span.setAttribute("timestamp", timestamp);
+        template.send(Constants.HEARTBEAT_TOPIC, timestamp);
+        span.addEvent("timestampSent");
+        span.end();
     }
 
     public static String extractHeartbeatFromRecord(ConsumerRecord<String, String> record) {
--- src/main/java/com/dynatrace/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/dynatrace/kafkademo/KafkaDemoApplication.java
@@ -1,21 +1,13 @@
 package com.dynatrace.kafkademo;
 
 import com.dynatrace.kafkademo.util.Constants;
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.Date;
-
+@EnableScheduling
 @SpringBootApplication
 public class KafkaDemoApplication {
 	public static void main(String[] args) {
