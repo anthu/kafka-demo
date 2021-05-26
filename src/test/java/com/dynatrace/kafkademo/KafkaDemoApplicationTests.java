a2de286a2da9dac25451794e8b402cf4dae114fa
Anton Huck <antonhuck@gmail.com>
2021-05-26 15:20:52 +0200

    Add EmbeddedKafka so gradle build works

--- src/test/java/com/dynatrace/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/dynatrace/kafkademo/KafkaDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.dynatrace.kafkademo;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class KafkaDemoApplicationTests {
+
+	@Autowired
+	VisitorsController visitorsController;
+
+	@Autowired
+	VisitorsController heartbeatController;
 
 	@Test
 	void contextLoads() {
+		assertThat(visitorsController).isNotNull();
+		assertThat(heartbeatController).isNotNull();
 	}
 
 }
